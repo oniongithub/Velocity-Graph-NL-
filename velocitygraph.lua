738ef63166142533a0b68cc2f261de103830729d
onion <62121953+oniongithub@users.noreply.github.com>
2021-08-22 22:40:28 -0400

    DPI Sizing

--- velocitygraph.lua
+++ velocitygraph.lua
@@ -2,6 +2,8 @@ local mousePos = Cheat.GetMousePos();
 local windowSize = EngineClient.GetScreenSize();
 local window = { x = 20, y = 20, w = 300, h = 125, down = false, downX = 0, downY = 0 };
 local velocityTable = { savedRealTime = GlobalVars.realtime, velocityGraph = {}, highestValue = 0, newHighestValue = 0 };
+local menuSize = Render.GetMenuSize();
+local dpi = menuSize.x / 800;
 
 --[[
     Input Library
@@ -81,13 +83,15 @@ local velocityEnable = Menu.Switch("Velocity", "Enabled", true);
 local velocityLineColor = Menu.ColorEdit("Velocity", "Line Color", Color.new(0.98, 0.45, 0.94, 1.0));
 local velocityHeaderColor = Menu.ColorEdit("Velocity", "Header Color", Color.new(0.98, 0.45, 0.94, 1.0));
 local velocityUpdate = Menu.SliderInt("Velocity", "Update Time (ms)", 20, 0, 500);
-local velocitySegments = Menu.SliderInt("Velocity", "Line Segments", 36, 5, 100);
 
 local xSlider = Menu.SliderInt("Position", "X Axis", 50, 0, windowSize.x, "", function(value) window.x = value; end);
 local ySlider = Menu.SliderInt("Position", "Y Axis", 50, 0, windowSize.y, "", function(value) window.y = value; end);
 
+
 local wSlider = Menu.SliderInt("Size", "Width", 300, 0, 800, "", function(value) window.w = value; end);
 local hSlider = Menu.SliderInt("Size", "Height", 125, 0, 500, "", function(value) window.h = value; end);
+local velocitySegments = Menu.SliderInt("Size", "Line Segments", 36, 5, 100);
+local velocityDPI = Menu.Switch("Size", "DPI Sizing", true);
 
 --[[
     Callbacks
@@ -97,6 +101,14 @@ keySystem.addKey(newKey(0x01));
 
 cheat.RegisterCallback("draw", function()
     local localPlayer = EntityList.GetLocalPlayer();
+    menuSize = Render.GetMenuSize();
+    dpi = menuSize.x / 800;
+
+    local windowW, windowH = window.w, window.h;
+    if (velocityDPI:Get()) then
+        windowW = windowW * dpi;
+        windowH = windowH * dpi;
+    end
 
     if (localPlayer ~= nil and EngineClient.IsInGame() and velocityEnable:Get()) then
         keySystem.run();
@@ -105,8 +117,8 @@ cheat.RegisterCallback("draw", function()
         mousePos = Cheat.GetMousePos();
 
         if (mouseKey.pressed.pressed) then
-            if (mouseKey.pressed.x >= window.x and mouseKey.pressed.x <= window.x + window.w) then
-                if (mouseKey.pressed.y >= window.y and mouseKey.pressed.y <= window.y + window.h) then
+            if (mouseKey.pressed.x >= window.x and mouseKey.pressed.x <= window.x + windowW) then
+                if (mouseKey.pressed.y >= window.y and mouseKey.pressed.y <= window.y + windowH) then
                     window.down, window.downX, window.downY = true, mouseKey.pressed.x - window.x, mouseKey.pressed.y - window.y;
                 end
             end
@@ -129,17 +141,17 @@ cheat.RegisterCallback("draw", function()
         local velText = tostring(velocity) .. " m/s";
         local textSize = Render.CalcTextSize(velText, 12)
 
-        Render.Blur(Vector2.new(window.x, window.y), Vector2.new(window.x + window.w, window.y + window.h), Color.new(1.0, 1.0, 1.0, 1.0));
-        Render.BoxFilled(Vector2.new(window.x, window.y), Vector2.new(window.x + window.w, window.y + 4), velocityHeaderColor:Get());
-        Render.Box(Vector2.new(window.x, window.y), Vector2.new(window.x + window.w, window.y + window.h), Color.new(0.2, 0.2, 0.2, 1.0));
-        Render.Box(Vector2.new(window.x, window.y + 4), Vector2.new(window.x + window.w, window.y + window.h - 4 - textSize.y), Color.new(0.2, 0.2, 0.2, 1.0));
+        Render.Blur(Vector2.new(window.x, window.y), Vector2.new(window.x + windowW, window.y + windowH), Color.new(1.0, 1.0, 1.0, 1.0));
+        Render.BoxFilled(Vector2.new(window.x, window.y), Vector2.new(window.x + windowW, window.y + 4), velocityHeaderColor:Get());
+        Render.Box(Vector2.new(window.x, window.y), Vector2.new(window.x + windowW, window.y + windowH), Color.new(0.2, 0.2, 0.2, 1.0));
+        Render.Box(Vector2.new(window.x, window.y + 4), Vector2.new(window.x + windowW, window.y + windowH - 4 - textSize.y), Color.new(0.2, 0.2, 0.2, 1.0));
 
         if (velocity > velocityTable.highestValue) then
             velocityTable.highestValue = velocity;
         end
 
         -- Manual Center bc we don't fuck w/ making custom fonts
-        Render.Text(velText, Vector2.new(window.x + (window.w / 2) - (textSize.x / 2), window.y + window.h - textSize.y - 2), Color.new(0.85, 0.85, 0.85, 1.0), 12);
+        Render.Text(velText, Vector2.new(window.x + (windowW / 2) - (textSize.x / 2), window.y + windowH - textSize.y - 2), Color.new(0.85, 0.85, 0.85, 1.0), 12);
 
         if (GlobalVars.realtime - velocityTable.savedRealTime >= velocityUpdate:Get() / 1000) then
             velocityTable.savedRealTime = GlobalVars.realtime;
@@ -176,13 +188,12 @@ cheat.RegisterCallback("draw", function()
         end
 
         if (#velocityTable.velocityGraph > 0) then
-            local boxHeight = (window.y + window.h - 8 - textSize.y) - (window.y + 10);
-            local lineWidth = (window.w - 2) / (#velocityTable.velocityGraph - 1);
+            local boxHeight = (window.y + windowH - 8 - textSize.y) - (window.y + 10);
+            local lineWidth = (windowW - 3) / (#velocityTable.velocityGraph - 1);
 
             local posTable = {};
             for i = 1, #velocityTable.velocityGraph do
                 local lineHeight = boxHeight * (velocityTable.velocityGraph[i] / velocityTable.highestValue);
-
                 table.insert(posTable, Vector2.new(window.x + 1 + (lineWidth * (i - 1)), window.y + 10 + (boxHeight - lineHeight)));
             end
 
